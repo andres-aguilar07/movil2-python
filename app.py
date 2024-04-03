88af9c63957d113243e658cb54e4573b153b7ad4
Yassed Matta <yassed883@gmail.com>
2024-04-03 08:22:36 -0500

    Update app.py with new Task API

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask, request, redirect, render_template
 from config.db import app
 
-#trabajar en las rutas de bluprint con respectos a las api's
+# trabajar en las rutas de bluprint con respectos a las api's
 from api.UserApi import ruta_user
 from api.CategoryApi import ruta_category
+from api.TaksApi import ruta_tak
 
+# Importar los Blueprints
 app.blueprints(ruta_user, url_prefix="/api")
 app.blueprints(ruta_category, url_prefix="/api")
+app.blueprints(ruta_category, url_prefix="/api")
 
-#config el servidor
+# config el servidor
+
 
 @app.route("/")
 def index():
     return "hola"
 
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(debug=True, port=5000, host="0.0.0.0")
