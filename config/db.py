65f2db0322f0c4137517b41343b8b4868a42c743
william <elhacker01@nose.com>
2024-04-08 07:55:58 -0500

    cambio

--- config/db.py
+++ config/db.py
@@ -4,13 +4,13 @@ from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/tasksul'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/tasksul'
 user = "winsignares"
 password = "admin123"
 direc = "winsignares.mysql.pythonanywhere-services.com"
-namebd = "tasksul"
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{user}:{password}@{direc}/{namebd}'
-app.config['SQLALCHEMY_TRACK_NOTIFICACTIONS'] = False
+namebd = "winsignares$tasksul"
+#app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{user}:{password}@{direc}/{namebd}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "Movil2"
 
 db = SQLAlchemy(app)
