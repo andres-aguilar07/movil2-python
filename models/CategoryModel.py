5e5881d660e2072038fb80458236d155892c610e
william <elhacker01@nose.com>
2024-04-10 08:19:53 -0500

    se arreglo el modelo category

--- models/CategoryModel.py
+++ models/CategoryModel.py
@@ -4,8 +4,8 @@ class Category(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     namecategory = db.Column(db.String(50))
     
-def __init__(self,namecategory):
-    self.namecategory = namecategory
+    def __init__(self,namecategory):
+        self.namecategory = namecategory
    
 with app.app_context():
     db.create_all()
